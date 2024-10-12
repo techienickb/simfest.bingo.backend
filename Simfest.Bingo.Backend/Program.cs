e5190cfceb44baaef5fb1c2dd0c530e092795131
Nick Brown <nick@nbdev.co.uk>
2024-10-13 00:23:10 +0100

    update connection string

--- Simfest.Bingo.Backend/Program.cs
+++ Simfest.Bingo.Backend/Program.cs
@@ -7,7 +7,7 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSignalR().AddAzureSignalR();
+builder.Services.AddSignalR().AddAzureSignalR(builder.Configuration["AZURE_SIGNALR_CONNECTIONSTRING"]);
 
 builder.Services.AddCors(options =>
 {
